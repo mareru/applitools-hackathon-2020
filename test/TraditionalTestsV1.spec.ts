aaaa313a71455ccc896ed3416926e2600e2a4243
MarijanaRukavina <marijana.rukavina@ecx.io>
2020-06-13 03:42:38 +0200

    Refactor

--- test/TraditionalTestsV1.spec.ts
+++ test/TraditionalTestsV1.spec.ts
@@ -18,123 +18,101 @@ describe('Task 1 - Cross Device Elements Test', () => {
 
     if (isMobile) {
         it('Search field NOT displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isFalse(reporter.writeRecord(1, testName, productListingPage.searchField.selector, productListingPage.searchField.isDisplayed(), false));
+            assert.isFalse(reporter.writeRecord(1, this.test!.title, productListingPage.searchField.selector, productListingPage.searchField.isDisplayed(), false));
         });
 
         it('Search icon NOT displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isFalse(reporter.writeRecord(1, testName, productListingPage.searchIcon.selector, productListingPage.searchIcon.isDisplayed(), false));
+            assert.isFalse(reporter.writeRecord(1, this.test!.title, productListingPage.searchIcon.selector, productListingPage.searchIcon.isDisplayed(), false));
         });
 
         it('Magnifying glass icon displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.magnifyingGlassIcon.selector, productListingPage.magnifyingGlassIcon.isDisplayed(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.magnifyingGlassIcon.selector, productListingPage.magnifyingGlassIcon.isDisplayed(), true));
         });
 
         it('Shopping cart quantity NOT displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isFalse(reporter.writeRecord(1, testName, productListingPage.shoppingCartQty.selector, productListingPage.shoppingCartQty.isDisplayed(), false));
+            assert.isFalse(reporter.writeRecord(1, this.test!.title, productListingPage.shoppingCartQty.selector, productListingPage.shoppingCartQty.isDisplayed(), false));
         });
     }
 
     if (isLaptop) {
         it('Main menu displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.mainMenu.selector, productListingPage.mainMenu.isDisplayed(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.mainMenu.selector, productListingPage.mainMenu.isDisplayed(), true));
         });
 
         it('Wishlist icon displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.wishlistIcon.selector, productListingPage.wishlistIcon.isDisplayed(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.wishlistIcon.selector, productListingPage.wishlistIcon.isDisplayed(), true));
         });
 
         it('Filters displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.filters.selector, productListingPage.filters.isDisplayed(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.filters.selector, productListingPage.filters.isDisplayed(), true));
         });
 
         it('Grid view icon displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.gridViewIcon.selector, productListingPage.gridViewIcon.isDisplayed(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.gridViewIcon.selector, productListingPage.gridViewIcon.isDisplayed(), true));
         });
 
         it('List view icon displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.listViewIcon.selector, productListingPage.listViewIcon.isDisplayed(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.listViewIcon.selector, productListingPage.listViewIcon.isDisplayed(), true));
         });
 
         it('All product tiles DO NOT have heart, cart, control shuffle icons displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.productTileHeartCartControlShuffleIcons.selector, productListingPage.allProductTilesDoNotHaveIcons(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.productTileHeartCartControlShuffleIcons.selector, productListingPage.allProductTilesDoNotHaveIcons(), true));
         });
     }
 
     if (isTablet) {
         it('Filters label displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.filtersLabel.selector, productListingPage.filtersLabel.isDisplayed(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.filtersLabel.selector, productListingPage.filtersLabel.isDisplayed(), true));
         });
     }
 
     if (isMobile || isTablet) {
         it('Main menu NOT displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isFalse(reporter.writeRecord(1, testName, productListingPage.mainMenu.selector, productListingPage.mainMenu.isDisplayed(), false));
+            assert.isFalse(reporter.writeRecord(1, this.test!.title, productListingPage.mainMenu.selector, productListingPage.mainMenu.isDisplayed(), false));
         });
 
         it('Wishlist icon NOT displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isFalse(reporter.writeRecord(1, testName, productListingPage.wishlistIcon.selector, productListingPage.wishlistIcon.isDisplayed(), false));
+            assert.isFalse(reporter.writeRecord(1, this.test!.title, productListingPage.wishlistIcon.selector, productListingPage.wishlistIcon.isDisplayed(), false));
         });
 
         it('Filters NOT displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isFalse(reporter.writeRecord(1, testName, productListingPage.filters.selector, productListingPage.filters.isDisplayed(), false));
+            assert.isFalse(reporter.writeRecord(1, this.test!.title, productListingPage.filters.selector, productListingPage.filters.isDisplayed(), false));
         });
 
         it('Grid view icon NOT displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isFalse(reporter.writeRecord(1, testName, productListingPage.gridViewIcon.selector, productListingPage.gridViewIcon.isDisplayed(), false));
+            assert.isFalse(reporter.writeRecord(1, this.test!.title, productListingPage.gridViewIcon.selector, productListingPage.gridViewIcon.isDisplayed(), false));
         });
 
         it('List view icon NOT displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isFalse(reporter.writeRecord(1, testName, productListingPage.listViewIcon.selector, productListingPage.listViewIcon.isDisplayed(), false));
+            assert.isFalse(reporter.writeRecord(1, this.test!.title, productListingPage.listViewIcon.selector, productListingPage.listViewIcon.isDisplayed(), false));
         });
 
         it('All product tiles have heart, cart, control shuffle icons displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.productTileHeartCartControlShuffleIcons.selector, productListingPage.allProductTilesHaveIcons(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.productTileHeartCartControlShuffleIcons.selector, productListingPage.allProductTilesHaveIcons(), true));
         });
     }
 
     if (isLaptop || isTablet) {
         it('Search field displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.searchField.selector, productListingPage.searchField.isDisplayed(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.searchField.selector, productListingPage.searchField.isDisplayed(), true));
         });
 
         it('Search icon displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.searchIcon.selector, productListingPage.searchIcon.isDisplayed(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.searchIcon.selector, productListingPage.searchIcon.isDisplayed(), true));
         });
 
         it('Magnifying glass icon NOT displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isFalse(reporter.writeRecord(1, testName, productListingPage.magnifyingGlassIcon.selector, productListingPage.magnifyingGlassIcon.isDisplayed(), false));
+            assert.isFalse(reporter.writeRecord(1, this.test!.title, productListingPage.magnifyingGlassIcon.selector, productListingPage.magnifyingGlassIcon.isDisplayed(), false));
         });
 
         it('Shopping cart quantity displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isTrue(reporter.writeRecord(1, testName, productListingPage.shoppingCartQty.selector, productListingPage.shoppingCartQty.isDisplayed(), true));
+            assert.isTrue(reporter.writeRecord(1, this.test!.title, productListingPage.shoppingCartQty.selector, productListingPage.shoppingCartQty.isDisplayed(), true));
         });
     }
 
     if (isMobile || isLaptop) {
         it('Filters label NOT displayed', function () {
-            const testName = this.test !== undefined ? this.test.title : 'Undefined test name';
-            assert.isFalse(reporter.writeRecord(1, testName, productListingPage.filtersLabel.selector, productListingPage.filtersLabel.isDisplayed(), false));
+            assert.isFalse(reporter.writeRecord(1, this.test!.title, productListingPage.filtersLabel.selector, productListingPage.filtersLabel.isDisplayed(), false));
         });
     }
 });
